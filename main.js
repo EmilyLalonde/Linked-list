8f3873a5701d414ab0a02dcd3250ebb20bdd05f6
Emily <lalonde0902@gmail.com>
2019-04-28 18:59:45 -0600

    Add toggle for read button and enter button is disabled

--- main.js
+++ main.js
@@ -6,57 +6,27 @@ var titleError= document.querySelector('.title-input-error');
 var urlError= document.querySelector('.url-input-error');
 var linkField = document.querySelector('.link-field')
 
-// window.addEventListener('load', buttonAbility);
-
-// function buttonAbility () {
-//   if (titleInput.value.length === 0 && urlInput.value.length === 0) {
-//     enterButton.disabled = true;
-// }else{
-//   enterButton.disabled = false;
-//   }
-// }
 
 
-
-// window.addEventListener('load', buttonEnable);
-
-// function buttonAbility () {
-//   if (titleInput.value === "" && urlInput.value === "") {
-//     enterButton.disabled = true;
-//     enterButton.classList.add('disabled');
-//   }else if (titleInput.value !== "" && urlInput.value !== ""){
-//     enterButton.disabled = false;
-//     enterButton.classList.remove('disabled');
-//   }
-// }
-
-// function buttonDisable () {
-//   if (titleInput.value === "" && urlInput.value === "") {
-//     enterButton.disabled = true;
-//     enterButton.classList.add('disabled');
-//   }
-// }
-
-// function buttonEnable () {
-//   if (titleInput.value !== "" && urlInput.value !== "") {
-//   enterButton.disabled = false;
-//   enterButton.classList.remove('disabled');
-//   }
-// }
-// card.addEventListener('click', function (){
-//   if(event.target.className === 'delete-btn') {
-//     event.target.parentNode.parentNode.remove();
-//   }
-//  });
-
-
- enterButton.addEventListener('click', createCard);
+window.addEventListener('keyup', enterButtonAbility);
+enterButton.addEventListener('click', enterButtonAbility);
+titleInput.addEventListener('keyup', enterButtonAbility);
+urlInput.addEventListener('keyup', enterButtonAbility);
+enterButton.addEventListener('click', errorMessage);
+enterButton.addEventListener('click', createCard);
+enterButton.addEventListener('click', cardCounter);
+linkField.addEventListener('click', read);
+linkField.addEventListener('click', function (e) {
+  if (e.target.className === 'delete-btn') {
+    e.target.parentElement.parentElement.remove();
+  }
+});
 
 function createCard (e) {
   e.preventDefault(e);
-  linkField.innerHTML +=
-  `<article class= "card">
-  <header class= "title-field">
+  var newCard =
+  `<article class= "card" id= "counter">
+  <header class= "title-field"> 
     <h2> ${titleInput.value}</h2>
   </header>
   <main class= "url-field">
@@ -68,30 +38,49 @@ function createCard (e) {
     <button class= "delete-btn">Delete</button>
   </footer>
 </article>`
+linkField.insertAdjacentHTML('afterbegin', newCard);
 clearInputs();
 }
+
+
  function clearInputs() {
   titleInput.value = "";
   urlInput.value = "";
  }
 
- linkField.addEventListener('click', function (e) {
-  if (e.target.className === 'delete-btn') {
-    e.target.parentElement.parentElement.remove();
+function errorMessage() {
+  if(titleInput.value === ""){
+    alert ('Please Enter a Valid Title');
   }
-});
+  if(urlInput.value === ""){
+    alert ('Please Enter a Valid URL');
+  }
+}
+
+function enterButtonAbility(e){
+  e.preventDefault()
+   if(titleInput.value === "" && urlInput.value === ""){
+     enterButton.disabled = true;
+     enterButton.classList.add('disabled')
+   } else {
+     enterButton.disabled = false;
+     enterButton.classList.remove('disabled')
+   }
+ };
+
+ function cardCounter() {
+  var countCards = document.getElementById('link-cards').childElementCount;
+  document.getElementById('card-count').innerHTML = countCards;
+}
 
 
-
-
-  
-// function errorMessage () {
-//   if (titleInput.value === '' || urlInput.value === '') {
-//   titleError.innerText = 'Please Enter a Website Title';
-//   urlError.innerText = 'Please Enter a Website URL';
-//   }else{
-//     titleError.innerText = '';
-//     urlError.innerText = '';
-//   }
-// }
+function read(e) {
+  var markRead = document.querySelector('.read-btn')
+  if (e.target.classList.contains('read') && e.target.classList.contains('read-btn')) {
+    markRead.classList.remove('read');
+  } else if (e.target.classList.contains('read-btn')) {
+    markRead.classList.add('read');
+    console.log('working', markRead);
+  }
+}
 
